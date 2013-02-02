1acabe89206aebe66b82bc4a34707fe23c8b30c3
Piotrek <piotrek@pxl.pl>
2013-02-02 10:27:01 +0100

    defaults udpate

--- src/views.js
+++ src/views.js
@@ -20,16 +20,32 @@ Generic = {
         events[name+" [data-action][data-on='"+name+"']"]='action'
     }
     return events
-}
+  },
+  selected:function(view){
+
+  }
 };
 
 Generic.defaults = {
-  events:Generic.actionEvents('mouseover','mousein','click','focus','dblclick'),
+  events:Generic.actionEvents('click','change','keyup'),
   action:Generic.action,
   // default events for model
   modelEvents: {
-    "change": "render",
-    "reset":"render"
+//    "change": "render"
+    "sync":"render"
+  },
+  bindListeners:function(){
+    var self = this;
+    _.each(this.listeners||{},function(listener,trigger){
+      if(!_.isFunction(listener)){
+        if(!_.isFunction(self[listener])){
+          throw 'listener '+listener+' is not callable';
+        }
+        listener = self[listener];
+      }
+      self.on(trigger,listener,self);
+
+    });
   },
   selector:function(){
     return this.$('[data-bind]:not([data-view][data-view!='+this.cid+'] [data-bind])');
@@ -80,7 +96,25 @@ Generic.ListView = Marionette.CompositeView.extend(_.defaults({
         }else{
             return this.collection;
         }
-    }
+    },
+    sort:function(e){
+      var $el =  $(e.target).closest('[data-order]')
+      var order = $el.data('order');
+      var field = $el.data('field');
+      if(order =='desc'){
+        field = '-'+field;
+        $el.data('order','asc')
+      }else{
+        $el.data('order','desc')
+      }
+
+      this.collection.refetch({data:{order:field}})
+    },
+    search:_.debounce(function(e){
+      var $el =  $(e.target).closest('[data-action]');
+      var value = $el.val() || undefined;
+      this.collection.refetch({data:{name__icontains:value}});
+    },100)
 },Generic.defaults));
 
 Generic.PageView = Marionette.Layout.extend(_.defaults({
@@ -91,6 +125,9 @@ Generic.PageView = Marionette.Layout.extend(_.defaults({
         main: "[data-region='main']",
         aside: "[data-region='aside']"
     },
+    modelEvents:{},
+//    modelEvents: {},
+  render: function(){ console.log("pageview render", this.cid); return Marionette.Layout.prototype.render.apply(this, arguments)},
     onRender: function(){
         if(this.mainView){
             this.main.show(new this.mainView(this.options));
@@ -102,43 +139,30 @@ Generic.PageView = Marionette.Layout.extend(_.defaults({
 },Generic.defaults));
 
 
-ListWidget = Generic.ListView.extend({
-    otherEvents:{},
-    constructor:function(options){
-        if(!options.collection && this.type){
-            this.collection = new this.type();
-        }
-        Generic.ListView.apply(this,arguments);
-        _.each(this.otherEvents,function(callback,event){
-             this.on(event,this[callback]);
-        }.bind(this));
-    }
-});
-
-
-Generic.MultipleSelectView = Generic.ListView.extend({
-  template: _.template('<select multiple></select>'),
-  itemViewContainer:'select',
-  itemViewOptions:function(item){
-    return {
-        selected:_.contains(this.options.selected||[],item.id)
-    }
-  },
-  itemView:Generic.ListItemView.extend({
-    template: _.template('<%= data.name||data.id %>'),
-    tagName:'option',
-    attributes:function(){
-      return {
-          value:this.model.id,
-          selected:(this.options.selected)
-      }
-    }
-  })
-});
+//
+//Generic.MultipleSelectView = Generic.ListView.extend({
+//  template: _.template('<select multiple></select>'),
+//  itemViewContainer:'select',
+//  itemViewOptions:function(item){
+//    return {
+//        selected:_.contains(this.options.selected||[],item.id)
+//    }
+//  },
+//  itemView:Generic.ListItemView.extend({
+//    template: _.template('<%= data.name||data.id %>'),
+//    tagName:'option',
+//    attributes:function(){
+//      return {
+//          value:this.model.id,
+//          selected:(this.options.selected)
+//      }
+//    }
+//  })
+//});
 
 
 Generic.AsideView = Marionette.CollectionView.extend({
-  widgets:[],
+    widgets:[],
   initialize:function(){
     this.collection = new Backbone.Collection(_.map(this.widgets,function(widget){return {view:widget}}));
   },
@@ -148,4 +172,64 @@ Generic.AsideView = Marionette.CollectionView.extend({
   itemViewOptions:function(){
     return this.options;
   }
+});
+
+
+
+MultipleSelectView = function(options){
+    var View = this.getView()
+    return new View(_.defaults(options,{selected:[]}));
+};
+
+MultipleSelectView.prototype.value = function(){
+    return this.options.selected;
+};
+
+
+MultipleSelectView.prototype.getView = function(){
+    var self = this;
+    return this.view.extend(
+      _.defaults(this.protoProps,{
+        itemViewOptions:function(){
+          var itemViewOptions = self.view.prototype.itemViewOptions || {};
+          if(_.isFunction(itemViewOptions)){
+            itemViewOptions = itemViewOptions.apply(this,arguments);
+          }
+          return _.extend({},itemViewOptions,this.options)
+        }
+      }),
+      this.staticProps);
+    };
+
+MultipleSelectView.extend = function(protoProps,staticProps){
+    var child = Backbone.View.extend.call(this, _.pick(protoProps||{},'view'));
+
+    child.prototype.protoProps  = _.extend({},
+        child.prototype.protoProps,
+        _.omit(protoProps,'view')
+    );
+    child.prototype.staticProps = _.extend({},
+        child.prototype.staticProps,
+        staticProps
+    );
+    return child;
+};
+
+
+Generic.MultipleSelectView = MultipleSelectView;
+
+
+
+
+Generic.ListWidget = Generic.ListView.extend({
+  constructor:function(options){
+    var options = _.clone(options)||{};
+    options.collection = new this.type();
+    Generic.ListView.call(this,options);
+  },
+  itemView:Generic.ListItemView.extend({
+    template:"#aside-message-list-item",
+    class:"media",
+    tagName:'li'
+  })
 });
